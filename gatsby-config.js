fd3844dd915b6b4cdb296e7e5193050f993f04a8
Jacob Beatty <jake@jacobbeatty.dev>
2021-02-22 20:42:28 +1030

    updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: "claudecreighton",
-    siteUrl: "https://claudecreighton.net"
+    siteUrl: "https://claudecreighton.art"
   },
   plugins: [
     {
