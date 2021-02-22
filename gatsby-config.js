8a21b30b372753c19368489e144f5f4f9b2a91ff
Jacob Beatty <jake@jacobbeatty.dev>
2021-02-22 20:08:20 +1030

    updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,7 +42,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        icon: "src/images/icon.png",
+        icon: "./src/images/icon.png",
       },
     },
     `gatsby-plugin-transition-link`,
